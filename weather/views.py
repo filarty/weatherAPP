a72778d2824bac8980d88e90d843294cb569e647
filarty <filarty@gmail.com>
2022-01-24 18:59:22 +0100

    update weather.views.py and urls.py

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+class Weather(View):
+    template_name = "index.html"
+
+    def get(self, request):
+        return render(request, template_name=self.template_name)
+
+
+
--- weatherAPP/urls.py
+++ weatherAPP/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from weather.views import Weather
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", Weather.as_view())
 ]
